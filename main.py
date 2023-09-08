c2f426f316a1e4b79abc8329408509506090c003
ansaam <elaf_ef@hotmail.com>
2023-09-09 00:23:11 +0300

    تم حل الواجب 6

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-# write your code here
+person ={
+    "name ":"ansam",
+    "age":15,
+    "hobbies":["coding, "playing vedio
+ games","painting" ]             
+}
+print(person["name"])
+print(person["age"])
+person["age"]=18
+person["country"]="kuwait"
+print(person)
+print("Length of dictionary  ,len(person)")
+ person["hobbies"]="swimming"
+ def check_hobbies(person):
+ if len(person["hobbies"])>=3:
+ print("wow you are amazing")  
+ else:
+ return False
+ ansam
+ 15
+ {'name':"ansam" , "age" :18, "hobbies"} :
+ ["coding" , "playing vedio games" , 'painting'],
+ 'country' :'kuwait'
+ length of dictionary 4
+
+    
+    
+   
+     
+
+
+
